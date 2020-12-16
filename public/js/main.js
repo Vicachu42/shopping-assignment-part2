c2de1d4e188fc72c323c111446817895269d7aba
Vic <victoria.b.graf@gmail.com>
2020-12-16 12:38:07 +0100

    Backup

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,11 @@
+const productsURL = 'http://localhost:8000/api/products';
+const cartsURL = 'http://localhost:8000/api/carts';
+
+
+
+let data = [];
+let cart = [];
+
+
 // fetch (localhost:8000)
 
--- server.js
+++ server.js
@@ -7,4 +7,10 @@ const port = 8000;
 const routes = require('./routes');
 app.use('/api', routes);
 
+app.use(express.static('public'));
+
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.237:5500');
+});
+
 app.listen(port);
